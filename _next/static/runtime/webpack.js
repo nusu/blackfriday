3f8ffac2a520aab5d8e872451a80c5f332f3a832
nusu alabuga <nusususuzu@gmail.com>
2018-11-22 15:31:59 +0300

    add prospect

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f6cf4274d2432047b287";
+/******/ 	var hotCurrentHash = "4d81c9e18b76c9bceb8e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
